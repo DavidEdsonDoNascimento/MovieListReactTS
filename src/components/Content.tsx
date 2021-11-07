8821dac77988f237fc44a31727809dcaef399872
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2021-11-07 15:02:05 -0300

    FEAT: main - Componentized content and sidebar, created separate Movie and Genre types in @types directory

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,25 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard'
+import { Movie } from './../@types/Movie'
+
+type ContentProps = {
+  genreTitle: string;
+  movies: Movie[]
+}
+
+export function Content({ genreTitle, movies }: ContentProps) {
+  return (
+    <>
+      <header>
+        <span className="category">Categoria:<span> {genreTitle}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,28 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button'
+import { Genre } from './../@types/Genre'
+
+type SideBarProps ={
+  selectedGenreId: number;
+  genres: Genre[];
+  handleClickButton: (id: number) => void;
+}
+export function SideBar({ selectedGenreId, genres, handleClickButton }: SideBarProps) {
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
